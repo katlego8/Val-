ada5ad73e0691e66c6370c5c48ddbcac53505939
katlego8 <126856192+katlego8@users.noreply.github.com>
2025-01-28 15:16:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'yes') {
+    if  (option === 'yes') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
@@ -11,7 +11,33 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'You sure?';
+    
+        // Function to get a random phrase for the "No" button
+        const getNoButtonText = () => {
+            const phrases = [
+                "No",
+                "Are you sure?",
+                "Really sure?",
+                "Think again!",
+                "Last chance!",
+                "Surely not?",
+                "You might regret this!",
+                "Give it another thought!",
+                "Are you absolutely certain?",
+                "This could be a mistake!",
+                "Have a heart!",
+                "Don't be so cold!",
+                "Change of heart?",
+                "Wouldn't you reconsider?",
+                "Is that your final answer?",
+                "You're breaking my heart ;(",
+            ];
+    
+            // Select a random phrase
+            return phrases[Math.floor(Math.random() * phrases.length)];
+        };
+
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
@@ -22,7 +48,7 @@ function selectOption(option) {
         alert('Invalid option!');
     }
 }
-
+/*
 // Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
@@ -39,6 +65,7 @@ function flashRainbowColors(callback) {
         }
     }, 2000); // Flash colors for 2 seconds
 }
+*/
 
 // Function to display the cat.gif initially
 function displayCat() {
@@ -47,7 +74,7 @@ function displayCat() {
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.src = 'Valentines day/cute-love-bear-roses-ou7zho5oosxnpo6k.gif'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
@@ -65,7 +92,7 @@ function displayCatHeart() {
     // Create a new Image element for the cat-heart
     var catHeartImage = new Image();
     // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
+    catHeartImage.src = 'Valentines day/bear-kiss-bear-kisses.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
     // Set alternative text for the image (for accessibility)
     catHeartImage.alt = 'Cat Heart';
     // When the cat-heart image is fully loaded, add it to the image container
